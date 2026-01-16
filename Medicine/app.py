02713ba794574ffa3d1a014a7cc8a79dcf1a0d67
Ankan Das <ankan.code45@gmail.com>
2026-01-16 23:26:05 +0530

    Update app.py

--- Medicine/app.py
+++ Medicine/app.py
@@ -75,3 +75,6 @@ def get_medicine_info():
 # --- Run ---
 if __name__ == "__main__":
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
